aaacff6c9325fc473feb7e98caeda2857e34f76a
dannehl <david@dannehl.net>
2021-03-25 14:22:29 +0100

    Updated Facade

--- src/ShipcloudFacade.php
+++ src/ShipcloudFacade.php
@@ -7,10 +7,14 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class ShipcloudFacade
  * @package Dannehl\Shipcloud
+ * @mixin \Dannehl\Shipcloud\Shipcloud
  */
 class ShipcloudFacade extends Facade
 {
 
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'shipcloud';
